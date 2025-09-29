2d3b2479fc7093f3595e4057d5bbc05fde8aaf72
Imrane Aabbou <imrane.aabbou@gmail.com>
2025-09-29 15:22:42 +0100

    feat: main menu added

--- main.py
+++ main.py
@@ -1,29 +1,8 @@
-from utils import input, radio, checkbox, select
+from services.main_menu import main_menu
 
 
 def main():
-    # 1️⃣ Normal text input
-    name = input("Enter your name:")
-    print(f"Hello, {name}!\n")
-
-    # 2️⃣ Single choice (radio)
-    favorite_lang = radio(
-        "Choose your favorite programming language:",
-        ["Python", "JavaScript", "Go", "Rust"],
-    )
-    print(f"Your favorite language is: {favorite_lang}\n")
-
-    # 3️⃣ Multiple choice (checkbox)
-    skills = checkbox(
-        "Select your programming skills:", ["Python", "JavaScript", "Go", "Rust"]
-    )
-    print(f"You selected these skills: {skills}\n")
-
-    # 4️⃣ Select menu
-    project = select(
-        "Choose a project to work on:", ["Project A", "Project B", "Project C"]
-    )
-    print(f"You selected project: {project}\n")
+    main_menu()
 
 
 if __name__ == "__main__":
