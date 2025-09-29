47d9d51a3c8aa45bc8874c5f0fb6097efc4c7273
Mohamed Machlou <medmachlou@gmail.com>
2025-09-29 15:22:50 +0100

    update concert service

--- services/concert.py
+++ services/concert.py
@@ -1,5 +1,4 @@
-from ..models import concert
-Concert = concert.Concert
+from ..models.concert import Concert
 
 # l'Ajout d'un nouveau concert
 def ajout_nouveau_concert(id_evenement, titre, date, lieu, capacite, artiste):
@@ -9,23 +8,30 @@ def ajout_nouveau_concert(id_evenement, titre, date, lieu, capacite, artiste):
 
 
 # Mis à jour de concert
-def update_concert(id_evenement, titre, date, lieu, capacite, artiste):
+def update_concert(id_evenement, date, lieu):
     concert_to_update = next((concert for concert in Concert.toutes_concerts if concert.id_evenement == id_evenement), None)
 
     if concert_to_update is not None:
-        concert_to_update.date = date
-        concert_to_update.lieu = lieu
+        if concert_to_update.date is not None:
+            concert_to_update.date = date
+        if concert_to_update.lieu is not None:
+            concert_to_update.lieu = lieu
         return concert_to_update
     else:
-        print(f'Aucun concert trouvé avec ID d\'Eveinement [{id_evenement}]!')
+        print(f'Aucun concert trouvé avec ID d\'Evenement [{id_evenement}]!')
+        return None
 
 
 
 # Supprimer d'un concert
 def delete_concert(id_evenement):
-    concert_to_detele = next((concert for concert in Concert.toutes_concerts if concert.id_evenement == id_evenement), None)
+    nombre_initial = len(Concert.toutes_concerts)
+    Concert.toutes_concerts = [concert for concert in Concert.toutes_concerts if concert.id_evenement != id_evenement]
 
-    if concert_to_detele is not None:
-        Concert.toutes_concerts = [concert for concert in Concert.toutes_concerts if concert.id_evenement != id_evenement]
+    if nombre_initial > len(Concert.toutes_concerts):
+        print(f'l\'Eveinement d\'ID: [{id_evenement}] a été supprimé!')
+        return True
     else:
         print(f'Aucun concert trouvé avec ID d\'Eveinement [{id_evenement}]!')
+        return False
+    
