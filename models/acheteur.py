01e2724cfa52d98df62a873b2b75e6ecd99e1f0d
Imrane Aabbou <imrane.aabbou@gmail.com>
2025-09-29 21:24:49 +0100

    feat: sync acheteurs in storage

--- models/acheteur.py
+++ models/acheteur.py
@@ -1,10 +1,47 @@
+from json import JSONDecodeError, dump, load
+from typing import Optional
+
+
 class Acheteur:
     achateurs = []
     _id = 1
 
-    def __init__(self, nom: str, email: str):
-        self.id_acheteur = self._id
+    def __init__(self, nom: str, email: str, _id: Optional[int] = None):
+        self.id_acheteur = self._id if _id is None else _id
         self.nom = nom
         self.email = email
         Acheteur.achateurs.append(self)
         Acheteur._id += 1
+        self._sync()
+
+    def delete(self):
+        Acheteur.achateurs.remove(self)
+        self._sync()
+
+    @classmethod
+    def _sync(cls):
+        with open("storage/acheteurs.json", "w+") as f:
+            dump(
+                list(map(lambda a: a.__dict__, cls.achateurs)),
+                f,
+                indent=4,
+                ensure_ascii=False,
+            )
+
+    @classmethod
+    def _load(cls):
+        try:
+            with open("storage/acheteurs.json") as f:
+                cls.achateurs = list(
+                    map(
+                        lambda a: Acheteur(a["nom"], a["email"], a["id_acheteur"]),
+                        load(f),
+                    )
+                )
+        except JSONDecodeError:
+            print(
+                "[!] Error while trying to load json data, the program is using empty data for now"
+            )
+
+
+Acheteur._load()
