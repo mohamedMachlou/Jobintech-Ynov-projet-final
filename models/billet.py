b8f175a4a0b3cc1fa51e1547ebc9be54c7a9853d
Mohamed Fejjary <mohamedfejjary@gmail.com>
2025-09-29 15:34:44 +0100

    difinition du constructeur de la classe Billet

--- models/billet.py
+++ models/billet.py
@@ -1,9 +1,17 @@
 # Classe billet
 class Billet:
-    prix = {
-        'Standard': 50,
-        'VIP':  100
+    billets = []
+
+    BILLET_TYPE = {
+        'Standard': .50,
+        'VIP':  .75
     }
+    billets = []
+    def __init__(self, prix, type_billet):
+        self.type_billet = type_billet
+        self.prix += prix * Billet.BILLET_TYPE[type_billet]
+        Billet.billets.append(self)
+
 
     def getPrix(self, type_billet):
         return self.prix.get(type_billet,0)
