5912ed4350bc93b25a64c4cc945dc9b343a21aec
Imrane Aabbou <imrane.aabbou@gmail.com>
2025-09-29 18:28:51 +0100

    feat: forwarding newly created acheteur to action menu

--- services/acheteur.py
+++ services/acheteur.py
@@ -8,5 +8,22 @@ def acheteur_registration():
     name = input("Nom:", validate=validate_required)
     email = input("Email:", validate=validate_acheteur_email)
 
-    acheteur = Acheteur(name, email)
-    # TODO: forward the achateur to a menu where they could buy billets
+    # creation + forwarding to the next menu
+    acheteur_action_selection(Acheteur(name, email))
+
+
+def acheteur_action_selection(acheteur: Acheteur):
+    from services.main_menu import main_menu
+
+    ACHETEUR_ACTION_SELECTION_CHOICES = {
+        "Buy Tickets": lambda: print("TODO: Buy"),
+        "Unbuy Tickets": lambda: print("TODO: unbuy"),
+        "Retour": main_menu,
+    }
+
+    choice = select(
+        " ",
+        choices=list(ACHETEUR_ACTION_SELECTION_CHOICES.keys()),
+    )
+
+    ACHETEUR_ACTION_SELECTION_CHOICES[choice]()
