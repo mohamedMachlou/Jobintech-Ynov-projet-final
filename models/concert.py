ff9894901968bde1f91b974bcb611bc59904bca4
Imrane Aabbou <imrane.aabbou@gmail.com>
2025-10-01 12:02:34 +0100

    fix: git conflict marks on concert model

--- models/concert.py
+++ models/concert.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from models import Evenement
 
 class Concert(Evenement):
@@ -6,26 +5,14 @@ class Concert(Evenement):
     # Fonction d'initialisation de Concert
     def __init__(self, id_evenement: int, titre: str, date: str, lieu: str, capacite: int, artiste: str):
         super().__init__(id_evenement, titre, date, lieu, capacite)
-=======
-from typing import Optional
-from models.evenement import Evenement
-
-
-class Concert(Evenement):
-    def __init__(
-        self,
-        titre: str,
-        date: str,
-        lieu: str,
-        capacite: int,
-        artiste: str,
-        _id: Optional[int] = None,
-        places_vendues: Optional[int] = None,
-    ):
->>>>>>> 1b98091c6d7ba2ad2f09dfc8d484efe2b4aa6c28
         self.artiste = artiste
-        super().__init__(titre, date, lieu, capacite, _id, places_vendues)
 
 
     def __str__(self):
         return super().__str__() + f" - Artiste: {self.artiste}"
+
+
+
+
+
+
