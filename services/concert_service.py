2b1217266a1c79589777b192a81210deced41c43
Mohamed Machlou <medmachlou@gmail.com>
2025-10-01 11:52:17 +0100

    update concert service

--- services/concert_service.py
+++ services/concert_service.py
@@ -1,43 +1,37 @@
 from models import Concert
 
-# l'Ajout d'un nouveau concert
-def ajout_nouveau_concert(id_evenement, titre, date, lieu, capacite, artiste):
-    new_concert = Concert(id_evenement, titre, date, lieu, capacite, artiste)
-    Concert.toutes_concerts.append(new_concert)
-    print('ajout works !!!!')
+# Ajout d'un nouveau concert
+def ajout_nouveau_concert(titre, date, lieu, capacite, artiste):
+    new_concert = Concert(titre, date, lieu, capacite, artiste)
+    print('Ajout réussi !')
     return new_concert
 
-
-# Mis à jour un concert
+# Mise à jour d'un concert
 def update_concert(id_evenement, date, lieu):
-    concert_to_update = next((concert for concert in Concert.toutes_concerts if concert.id_evenement == id_evenement), None)
+    concert_to_update = next(
+        (concert for concert in Concert.tous_concerts if concert.id_evenement == id_evenement),
+        None
+    )
 
     if concert_to_update is not None:
         concert_to_update.date = date
         concert_to_update.lieu = lieu
+        concert_to_update._sync()  # mettre à jour le fichier JSON
         return concert_to_update
     else:
         print(f'Aucun concert trouvé avec ID d\'Evenement [{id_evenement}]!')
         return None
 
-
 # Supprimer un concert
 def delete_concert(id_evenement):
-    nombre_initial = len(Concert.toutes_concerts)
-    Concert.toutes_concerts = [concert for concert in Concert.toutes_concerts if concert.id_evenement != id_evenement]
-
-    if nombre_initial > len(Concert.toutes_concerts):
-        print(f'l\'Evenement d\'ID: [{id_evenement}] a été supprimé!')
+    concert_to_delete = next(
+        (concert for concert in Concert.tous_concerts if concert.id_evenement == id_evenement),
+        None
+    )
+    if concert_to_delete:
+        concert_to_delete.delete()  # utilise la méthode delete du modèle
+        print(f'Evenement d\'ID [{id_evenement}] supprimé !')
         return True
     else:
         print(f'Aucun concert trouvé avec ID d\'Evenement [{id_evenement}]!')
         return False
-
-
-
-
-
-
-
-
-
