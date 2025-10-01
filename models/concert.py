4bc519ed90fcdc9224b86e6278c31f5859afb5af
Imrane Aabbou <imrane.aabbou@gmail.com>
2025-10-01 12:06:50 +0100

    fix: update concert model content to latest content

--- models/concert.py
+++ models/concert.py
@@ -1,18 +1,21 @@
-from models import Evenement
+from typing import Optional
+from models.evenement import Evenement
+
 
 class Concert(Evenement):
 
-    # Fonction d'initialisation de Concert
-    def __init__(self, id_evenement: int, titre: str, date: str, lieu: str, capacite: int, artiste: str):
-        super().__init__(id_evenement, titre, date, lieu, capacite)
+    def __init__(
+        self,
+        titre: str,
+        date: str,
+        lieu: str,
+        capacite: int,
+        artiste: str,
+        _id: Optional[int] = None,
+        places_vendues: Optional[int] = None,
+    ):
         self.artiste = artiste
-
+        super().__init__(titre, date, lieu, capacite, _id, places_vendues)
 
     def __str__(self):
         return super().__str__() + f" - Artiste: {self.artiste}"
-
-
-
-
-
-
